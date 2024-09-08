b762cd4a1722f34a0a28d6570d20f6d685ea4961
nameisnone <nameisnone@xx.com>
2024-09-08 18:28:24 +0800

    feat: 添加树莓派交叉编译工具链包描述

---
+++ packages/r/rpi_toolchain/xmake.lua
@@ -0,0 +1,26 @@
+package("rpi_toolchain")
+
+    set_kind("toolchain")
+    set_homepage("https://github.com/raspberrypi/tools")
+    set_description("raspberrypi toolchain")
+
+    local version_map = {
+        ["2021.11"] = "439b6198a9b340de5998dd14a26a0d9d38a6bcac"
+    }
+
+    if is_host("linux") then
+        set_urls("https://github.com/raspberrypi/tools.git")
+        add_versions("2021.11", "439b6198a9b340de5998dd14a26a0d9d38a6bcac")
+
+    end
+
+    on_install("@linux", function (package)
+        os.cp("arm-bcm2708/arm-linux-gnueabihf", package:installdir() .. "/arm-linux-gnueabihf")
+    end)
+
+    on_test(function (package)
+        local gcc = "arm-linux-gnueabihf-gcc"
+        os.exec("pwd")
+        os.vrunv(gcc, {"--version"})
+    end)
+
